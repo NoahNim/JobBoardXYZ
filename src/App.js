0d5b0edb5a0020e9df2ec16427ed682791feabc5
noahnim <nmedoff123@gmail.com>
2022-06-23 22:36:13 -0700

    clean up test code

--- src/App.js
+++ src/App.js
@@ -3,39 +3,7 @@ import db from "./firebase";
 import { doc, setDoc } from "firebase/firestore";
 
 function App() {
-  const [customerName, setCustomerName] = useState("");
-  const [customerPassword, setCustomerPassword] = useState("");
-
-  const submit = async (e) => {
-    e.preventDefault();
-    await setDoc(doc(db, "customerData", "customers"), {
-      name: customerName,
-      password: customerPassword,
-    });
-
-    setCustomerName("");
-    setCustomerPassword("");
-  };
-
-  return (
-    <div className="App">
-      <div className="App__form">
-        <input
-          type="text"
-          placeholder="Name"
-          value={customerName}
-          onChange={(e) => setCustomerName(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="Password"
-          value={customerPassword}
-          onChange={(e) => setCustomerPassword(e.target.value)}
-        />
-        <button onClick={submit}>Submit</button>
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
