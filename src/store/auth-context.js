80397d5f4fd8eb283e2dcdb62349e547ca5af646
noahnim <nmedoff123@gmail.com>
2022-06-28 01:47:22 -0700

    remove unused vars

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext, createContext } from "react";
+import React, { useState, createContext } from "react";
 import { auth } from "../firebase";
 import {
   signInWithEmailAndPassword,
